bf714eafbb7afc26de7aaa4ba98115f68672fddf
grieblerremodeling <120071581+grieblerremodeling@users.noreply.github.com>
2022-12-11 20:09:11 -0300

    Update apiClient.js

--- src/pages/api/apiClient.js
+++ src/pages/api/apiClient.js
@@ -7,7 +7,7 @@ export function setupAPIClient(ctx = undefined) {
   let cookies = parseCookies(ctx);
 
   const api = axios.create({
-    baseURL: "https://grieblerremodeling.com/",
+    baseURL: "https://www.grieblerremodeling.com/",
     //baseURL: "http://localhost:3000/",
     headers: {
       Authorization: `Bearer ${cookies["@nextauth.token"]}`,
